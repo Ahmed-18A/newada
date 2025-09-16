2ef3850f7cd38ac6b72dc8ec6e043fd81b0cfacf
PC-12 <ahmed.abu.21418@gmail.com>
2025-09-16 09:47:19 +0300

    amamamam

--- app/src/main/java/com/example/men2/MainActivity2.java
+++ app/src/main/java/com/example/men2/MainActivity2.java
@@ -1,6 +1,7 @@
 package com.example.men2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,22 +24,46 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         EditText name = findViewById(R.id.name);
         EditText pass = findViewById(R.id.pass);
+        Button sign=findViewById(R.id.sign);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = name.getText().toString().trim();
+                String password = pass.getText().toString().trim();
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity2.this, "Please fill in both fields", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    SharedPreferences share=getSharedPreferences("UserPrefs",MODE_PRIVATE);
+                    SharedPreferences.Editor edetor = share.edit();
+
+                    edetor.putString("email",username);
+                    edetor.putString("pass",password);
+                    edetor.apply();
+                }
+            }
+        });
         Button log = findViewById(R.id.log);
         log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = name.getText().toString();
-                String password = pass.getText().toString();
+                SharedPreferences share=getSharedPreferences("UserPrefs",MODE_PRIVATE);
+                String saved_username = share.getString("email",null);
+                String saved_password = share.getString("pass",null);
+
+                String username = name.getText().toString().trim();
+                String password = pass.getText().toString().trim();
                 if (username.isEmpty() || password.isEmpty()) {
                     Toast.makeText(MainActivity2.this, "Please fill in both fields", Toast.LENGTH_SHORT).show();
                 } else {
-                    if (username.equals("admin") && password.equals("admin")) {
+                    if (saved_username.equals(username)&&saved_password.equals(password)) {
                         Toast.makeText(MainActivity2.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
                         intent.putExtra("name", username);
                         startActivity(intent);
-                    } else {
-                        Toast.makeText(MainActivity2.this, "wrong info", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(MainActivity2.this, "sign up first", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
